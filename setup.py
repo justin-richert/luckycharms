680c36404f2f6390cbed8ddb57ec59681a54c4d3
Justin Richert <justin.richert@life.church>
2020-06-24 09:38:01 -0500

    Update version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='luckycharms',
-    version='0.5.9',
+    version='0.5.10',
     url='https://github.com/justin-richert/luckycharms',
     download_url='https://github.com/justin-richert/luckycharms/archive/0.5.1.zip',
     license='MIT',
